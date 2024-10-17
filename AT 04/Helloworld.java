3b3f436cd0edc7a7a902028f8411bda4f889f2f3
rnjukes <25rjukes@derryfield.org>
2024-10-16 20:06:28 -0400

    Create Helloworld.java

---
+++ AT 04/Helloworld.java
@@ -0,0 +1,9 @@
+//Ryan Jukes
+//10-16-24
+//AT 04 Hello World Demo
+
+public class Helloworld {
+    public static void main(String[] args) throws Exception  {
+        System.out.println("Hello World");
+    }
+}
