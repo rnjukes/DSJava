24408582390143c18fe2511153792d9e2a3aac4a
rnjukes <25rjukes@derryfield.org>
2024-10-24 12:05:35 -0400

    AT05

---
+++ AT 05/JukesAT05A.java
@@ -0,0 +1,21 @@
+//Ryan Jukes
+//10-23-24
+//AT 05
+
+import java.util.Random;
+import java.util.Scanner;
+import java.util.random.*;
+public class JukesAT05 {
+    public static void main(String[] args) throws Exception  {
+        Scanner in = new Scanner(System.in);
+
+        //Part A
+        Random randomNumb = new Random();
+
+        for (int refNumb = 0; refNumb < 100; refNumb++) {
+            int trueRandomNumb = randomNumb.nextInt(100);
+            System.out.println(trueRandomNumb);
+        }
+        
+    }
+}
---
+++ AT 05/JukesAT05B.java
@@ -0,0 +1,14 @@
+//Ryan Jukes
+//10-23-24
+//AT 05
+
+import java.util.Scanner;
+public class JukesAT05B {
+    public static void main(String[] args) throws Exception  {
+        Scanner in = new Scanner(System.in);
+
+        //Part B
+        
+
+    } 
+}
