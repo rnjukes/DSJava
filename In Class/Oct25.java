39fe2874b1e695fc99914bf748a5a1c67be580fa
rnjukes <25rjukes@derryfield.org>
2024-10-25 11:58:18 -0400

    In Class

---
+++ In Class/Oct25.java
@@ -0,0 +1,21 @@
+//In Class October 25th
+
+import java.util.Scanner;
+public class Oct25 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in); 
+        double total = 0;
+        double current = 0; 
+        do {
+            //Negative value will quit program
+        System.out.println("Enter a dollar amount");
+        if (in.hasNextDouble()) {
+
+        }
+    } while () 
+
+    }
+
+   // 
+
+}
