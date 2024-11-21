adc601fe9f1f4daf8768a2aea9a0cb5927a868c7
rnjukes <25rjukes@derryfield.org>
2024-11-21 16:50:35 -0500

    -

---
+++ HW14/JukesHW14A.java
@@ -0,0 +1,14 @@
+/*
+ * Ryan Jukes
+ * 11-21-24
+ * AT Java HW14 Part A
+ * 
+ *  1)
+ *  Answer B - 2
+ * 
+ *  2) 
+ *  Answer C - array[5] = 1;
+ * 
+ *  3)
+ *  Answer C - 4 
+ */
---
+++ HW14/JukesHW14B.java
@@ -0,0 +1,15 @@
+/*
+ * Ryan Jukes
+ * 11-21-24
+ * AT Java HW14 Part B
+ */
+
+public class JukesHW14B {
+    public static void main(String[] args) {
+        double[] numbs = { 2.2, 1.0, 3.7, 4.1, 88.0 };
+        
+        for (double i : numbs) {
+            System.out.println(i);
+        }
+    }
+}
