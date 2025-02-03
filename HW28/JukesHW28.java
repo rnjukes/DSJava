3e3ee028c1128e42991c9adaa35462af363df9d3
rnjukes <25rjukes@derryfield.org>
2025-02-03 10:45:57 -0500

    -

---
+++ HW28/JukesHW28.java
@@ -0,0 +1,17 @@
+package HW28;
+/*
+ *  Ryan Jukes
+ *  2-3-25
+ *  AT Java HW 28 Part A + B
+ * 
+ *  Part A
+ *  In the context of snakes, crocodiles, reptiles, and turtles, reptiles are the superclass. It is the most
+ *  general class amoung them all. All of the other options are reptiles, but for example, not all reptiles are
+ *  snakes, making reptiles the superclass among the options available. 
+ * 
+ *  Part B
+ *  AcousticGuitar ag; and ElectricGuitar eg; are subclasses of Guitar, so they can be passed. Guitar myGuitar; is 
+ *  expected and can be passed as well. Instrument myInstrument; cannot be passed because it is the superclass of 
+ *  Guitar, and as we learned in class, it would not be able to be passed going the wrong direction logically. 
+ * 
+ */
