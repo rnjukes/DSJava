a5efe5fa6dc69094de8fe6432eee25a60d88d4b9
rnjukes <25rjukes@derryfield.org>
2025-04-01 09:29:22 -0400

    -

---
+++ NO1/JukesNO1.java
@@ -0,0 +1,17 @@
+package NO1;
+
+/*
+Ryan Jukes
+4-1-25
+NO1 Part A + B
+
+A-
+Laurie's smartphone sends data to the needed network wuth a specific identification. The information is sent, and is verfied that it is 
+uncorrupted with its specific number. This process would use TCP.
+
+B-
+When Jerry launches the game, his computer sends updates to the server about his inputs, and the server responds with output of its own
+to feed to Jerry's computer. The server does all of the processing for the interworkings of the game, and the computer verifies that the 
+signals aren't courrupted when they're recieved. This process would use UDP.
+
+*/
